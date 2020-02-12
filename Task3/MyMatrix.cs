ea2042c8163778badc8d1c997911a9474a52b8f5
Loarlam <messiah02@mail.ru>
2020-02-12 21:32:24 +0300

    Add project files.

---
+++ Task3/MyMatrix.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task3
+{
+    class MyMatrix
+    {
+        int[,] _array;
+
+        public MyMatrix(int[,] filledArray)
+        {
+            _array = filledArray;
+        }
+
+        public int this[int index1, int index2] => _array[index1, index2];
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,48 @@
+﻿/*Используя Visual Studio, создайте проект по шаблону Console Application.  
+Требуется:  
+Создать класс MyMatrix, обеспечивающий представление матрицы произвольного размера 
+с возможностью изменения числа строк и столбцов.  
+Написать программу, которая выводит на экран матрицу и производные от нее матрицы разных порядков. 
+*/
+using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array;
+
+            Console.Write("Количество строк матрицы = ");
+            int numberOfLines = Int32.Parse(Console.ReadLine());
+            Console.Write("Количество столбцов матрицы = ");
+            int numberOfColumns = Int32.Parse(Console.ReadLine());
+
+            array = new int[numberOfLines, numberOfColumns];
+
+            Random randomIntValue = new Random();
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                for (int j = 0; j < numberOfColumns; j++)
+                {
+                    array[i, j] = randomIntValue.Next(10, 99);
+                }
+            }
+
+            MyMatrix myMatrix = new MyMatrix(array);
+
+            Console.WriteLine("Элементы матрицы:");
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                for (int j = 0; j < numberOfColumns; j++)
+                {
+                    Console.Write($"{myMatrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
